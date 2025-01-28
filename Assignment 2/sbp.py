48eec87ccac875059d93386294c9f1ea3bf06527
Saksham Rajbhandari <saksham726019@gmail.com>
2025-01-28 03:48:40 -0500

    check for available moves. Need to implement a separate check for the masterbrick

--- Assignment 2/sbp.py
+++ Assignment 2/sbp.py
@@ -5,11 +5,14 @@ class SlidingBrick:
         self.__width: int = w
         self.__height: int = h
         self.__board: list = board
+        self.__emptyCells: list = []  # Store (row/height, column/width) tuple in this list.
         self.__masterBrickPosition: int = 0
     
+    # Function to return the board.
     def getBoard(self) -> list:
         return self.__board
     
+    # Function to print the board.
     def printBoard(self) -> None:
         print(f"{self.__width},{self.__height},")
 
@@ -29,6 +32,61 @@ class SlidingBrick:
                 return False
                     
         return True
+    
+    # Function to find empty cells (0) in the board.
+    def findEmptyCells(self) -> None:
+        for h in range(self.__height):
+            for w in range(self.__width):
+                if self.__board[h][w] == 0:
+                    self.__emptyCells.append((h, w))
+    
+    # Function to return the empty cells location.
+    def getEmptyCells(self) -> list:
+        self.__emptyCells.clear()
+        self.findEmptyCells()
+        return self.__emptyCells
+    
+    # Function to get the available moves based on the empty cells position.
+    def getMoves(self) -> list:
+        moves: list = []    # We'll store valid moves in this list.
+
+        # Directions (up, down, left, right)
+        directions: dict = {
+            "up" : (-1, 0),
+            "down" : (1, 0),
+            "left" : (0, -1),
+            "right" : (0, 1)
+        }
+
+        opposite_directions: dict = {
+            "up" : "down",
+            "left" : "right",
+            "right" : "left",
+            "down" : "up"
+        }
+
+        # Get the empty cells location.
+        emptyCells: list = self.getEmptyCells()
+
+        # We will check each direction and add whatever is valid to the moves list. Implement the check for masterbrick.
+        for h, w in emptyCells:
+            for direction, (row, column) in directions.items():
+                new_row, new_column = (h + row), (w + column)
+
+                # Check if new row and column are within bounds.
+                if (0 <= new_row < self.__height) and (0 <= new_column < self.__width):
+                    # Check if the location is wall or not (1).
+                    if (self.__board[new_row][new_column] > 0) and (self.__board[new_row][new_column] != 1):
+                        # Check if the location is not a master brick.
+                        if (self.__board[new_row][new_column] != 2):
+                            moves.append((self.__board[new_row][new_column], opposite_directions[direction]))
+
+                            # If the location is master brick, we can only move certain direction based on how master brick is on the board.
+                        elif (self.__board[new_row][new_column] == 2):
+                            pass
+
+        
+        return moves
 
 
 # Function to copy and return the initial (original) board.
@@ -39,7 +97,6 @@ def cloneOriginalBoard(sliding_brick: SlidingBrick) -> list:
         original_board.append(row[:])
 
     return original_board
-
 
 # Function to load the game from the file and create the Sliding Brick instance and then print the board.
 def loadGame(filename) -> SlidingBrick:
@@ -67,7 +124,6 @@ def loadGame(filename) -> SlidingBrick:
     sliding_brick = SlidingBrick(width, height, board)
     return sliding_brick
 
-
 # Main function.
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -78,7 +134,7 @@ if __name__ == "__main__":
 
     if command == "print":
         if len(sys.argv) < 3:
-            print("Error: Missing filename for 'print' command.")
+            print(f"Error: Missing filename for {command} command.")
             sys.exit(1)
 
         filename: str = sys.argv[2]
@@ -91,7 +147,7 @@ if __name__ == "__main__":
 
     elif command == "done":
         if len(sys.argv) < 3:
-            print("Error: Missing filename for 'done' command.")
+            print(f"Error: Missing filename for {command} command.")
             sys.exit(1)
 
         filename: str = sys.argv[2]
@@ -101,6 +157,23 @@ if __name__ == "__main__":
 
         # Check if we are in goal state.
         print(sliding_brick.isGoalState())
+
+    elif command == "availableMoves":
+        if len(sys.argv) < 3:
+            print(f"Error: Missing filename for {command} command.")
+            sys.exit(1)
+
+        filename: str = sys.argv[2]
+
+        # Load the game.
+        sliding_brick: SlidingBrick = loadGame(filename)
+
+        # Get the valid moves.
+        moves: list = sliding_brick.getMoves()
+
+        # Print the moves.
+        for move in moves:
+            print(f"({move[0]}, {move[1]})")
 
     else:
         print(f"Error: Unknown command '{command}'.")
@@ -116,3 +189,5 @@ if __name__ == "__main__":
     #         items += str(item) + ", "
     #     print(items)
 
+    # Test. Remove later
+    # print(f"\nEmpty cells: {sliding_brick.getEmptyCells()}")
